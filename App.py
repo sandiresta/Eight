fa298601bc4905de297e2fd155ee35b1898df706
sandiresta <atseridnas23@gmail.com>
2024-12-14 14:00:43 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="Image Processing App", page_icon="https://github.com/sandiresta/Eight/blob/main/assets/logo%20pu.png")
+st.set_page_config(page_title="Image Processing App", page_icon="assets/logo%20pu.png")
 
 st.sidebar.title("Navigation")
 st.sidebar.write("Use the sidebar to navigate through the app.")
 
 st.write("# Welcome to the Image Processing App")
-st.image("https://github.com/sandiresta/Eight/blob/main/assets/logo%20pu.png", width=200, caption="Our Beloved University")
+st.image("assets/logo%20pu.png", width=200, caption="Our Beloved University")
 st.write("This is the main page. Use the menu on the left to explore different sections.")
