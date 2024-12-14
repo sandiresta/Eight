2b2f7eac8e6582aefd41d5da948a7bcfcffdf99a
sandiresta <atseridnas23@gmail.com>
2024-12-14 13:50:23 +0700

    Update 2_About_Group.py

--- pages/2_About_Group.py
+++ pages/2_About_Group.py
@@ -4,7 +4,7 @@ st.title("About Our Group")
 st.write("Meet the amazing members of our group!")
 
 group_members = [
-    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\WhatsApp Image 2024-12-13 at 21.05.38_b1b3ada7.png"},
+    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"https://github.com/sandiresta/Eight/blob/main/assets/WhatsApp%20Image%202024-12-13%20at%2021.05.38_b1b3ada7.png"},
     {"Name": "Sandi Resta", "Role": "DM ZP Leader", "Email": "sandi.resta@michelin.com", "Image": r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\20220822_211922.png"},
     {"Name": "Raden Asep Ahmad Fadillah", "Role": "2W BU Leader", "Email": "raden.fadillah@michelin.com", "Image": r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\WhatsApp Image 2024-12-13 at 21.06.02_107ccee3.png"},
 ]
