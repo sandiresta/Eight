8659ed95cd86bd6db659911ae35067a73c7474df
sandiresta <atseridnas23@gmail.com>
2024-12-14 13:59:24 +0700

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Linear Algebra Project Group 8")
-st.image("https://github.com/sandiresta/Eight/blob/main/assets/logo%20pu.png", width=200, caption="Our Beloved University")
+st.image("assets/logo%20pu.png", width=200, caption="Our Beloved University")
 st.write("This Application is to fullfil Final Project of Linear Algebra, Created by Group 8, Hopefully It will be usefull for the future.")
