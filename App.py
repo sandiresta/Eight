e172f8a755bb1a151e676d90776244043c12c914
sandiresta <atseridnas23@gmail.com>
2024-12-15 14:02:24 +0700

    Update App.py

--- App.py
+++ App.py
@@ -7,11 +7,7 @@ st.sidebar.title("Navigation")
 st.sidebar.write("Use the sidebar to navigate through the app.")
 
 st.write("# Welcome to the Image Processing App")
-try:
 img = Image.open(r"https://github.com/sandiresta/Eight/blob/main/assets/logo%20pu.png")
-img.verify()
-except exception as e:
-st.error(f"error:{e}")
 
 st.image(img, use_column_width=True, caption="Our Beloved University")
 st.write("This is the main page. Use the menu on the left to explore different sections.")
