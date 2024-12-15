6a435c1e86e6ceaf75f8c3ce3f191385b2f5b67a
sandiresta <atseridnas23@gmail.com>
2024-12-15 13:29:10 +0700

    Update 2_About_Group.py

--- pages/2_About_Group.py
+++ pages/2_About_Group.py
@@ -4,7 +4,7 @@ st.title("About Our Group")
 st.write("Meet the amazing members of our group!")
 
 group_members = [
-    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"assets/WhatsApp%20Image%202024-12-13%20at%2021.05.38_b1b3ada7.png"},
+    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"https://github.com/sandiresta/Eight/blob/main/assets/WhatsApp%20Image%202024-12-13%20at%2021.05.38_b1b3ada7.png"},
     {"Name": "Sandi Resta", "Role": "DM ZP Leader", "Email": "sandi.resta@michelin.com", "Image": r"assets/20220822_211922.png"},
     {"Name": "Raden Asep Ahmad Fadillah", "Role": "2W BU Leader", "Email": "raden.fadillah@michelin.com", "Image": r"assets/WhatsApp%20Image%202024-12-13%20at%2021.06.02_107ccee3.png"},
 ]
