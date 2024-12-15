b94bbad0d99ee45537dcf3da044899721633213b
sandiresta <atseridnas23@gmail.com>
2024-12-15 14:03:50 +0700

    Update App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ st.sidebar.title("Navigation")
 st.sidebar.write("Use the sidebar to navigate through the app.")
 
 st.write("# Welcome to the Image Processing App")
-img = Image.open(r"https://github.com/sandiresta/Eight/blob/main/assets/logo%20pu.png")
+img=Image.open(r"https://github.com/sandiresta/Eight/blob/main/assets/logo%20pu.png")
 
 st.image(img, use_column_width=True, caption="Our Beloved University")
 st.write("This is the main page. Use the menu on the left to explore different sections.")
